022440cb7337b7bcbeb5ea96094f913d1ea28ed4
David Allan <Davida713@gmail.com>
2013-07-03 16:11:41 -0500

    Add status validation tests

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -17,13 +17,36 @@ class StatusesControllerTest < ActionController::TestCase
     assert_redirected_to new_user_session_path
   end
 
+  test "should be logged in to post a status" do
+    post :create, status: { content: "Hello" }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+
+  end
+
+
+  test "user should get edit when logged in" do
+    sign_in users (:David)
+
+    get :edit, id: @status
+    assert_response :success
+  end
+
   test "Should render new page when logged in" do
     sign_in users (:David)
     get :new
     assert_response :success
   end
 
-  test "should create status" do
+  test "should redirect status update when not logged in" do
+    put :update, id: @status, status: { content: @status.content}
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should create status when logged in" do
+    sign_in users (:David)
+
     assert_difference('Status.count') do
       post :create, status: { content: @status.content}
     end
